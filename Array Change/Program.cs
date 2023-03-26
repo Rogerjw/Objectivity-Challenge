f149b8858090363d9221aad7b790cb8162700ecb
rogerm@techaffinity.com <rogerm@techaffinity.com>
2023-03-26 10:33:30 +0200

    Add the Array Change challenge

---
+++ Array Change/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Array_Change
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] originalArray = { "Roger", "Muhire", 25 };
+            dynamic[] updatedArray = { "Muhire", 1.88 };
+            var (newElements, removedElements) = ArrayChange(originalArray, updatedArray);
+            Console.WriteLine($"New elements: {string.Join(", ", newElements)}");
+            Console.WriteLine($"Removed elements: {string.Join(", ", removedElements)}");
+            Console.ReadKey();
+        }
+
+        static public (dynamic[], dynamic[]) ArrayChange<dynamic>(dynamic[] originalArray, dynamic[] updatedArray)
+        {
+            dynamic[] newElements = updatedArray.Except(originalArray).ToArray();
+            dynamic[] removedElements = originalArray.Except(updatedArray).ToArray();
+            return (newElements, removedElements);
+        }
+    }
+}
